bd2e1ef67aeeeec40a0f2f508a023c5583532564
Salman Alam Siddiqi <salman-alam.siddiqi@t-systems.com>
2022-08-11 11:21:42 +0200

    latest update

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@
             @keypress="fetchWeather"
           />
         </div>
+        <div class="logoDiv">
+          
+        </div>
         <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
           <div class="location-box">
             <div class="location">
@@ -66,15 +69,10 @@ export default {
     };
   },
   mounted() {
-    let color = "#";
-    //let colorCode = ["ffaf40", "2A9BFB", "ff5967", "7870cc", "33ccbf"]; // colors
     let colorCode = ["112, 101, 99, 0.7", "149, 35, 14, 0.7", "24, 48, 24, 0.7", "11, 203, 247, 0.7", "163, 203, 41, 0.7"]; // colors
     let className = document.getElementsByClassName("change"); // class name to random color
-    let i;
-    color += colorCode[Math.floor(Math.random() * colorCode.length)];
-    console.log(color);
     let randomNumber = Math.floor(Math.random() * 5);
-    for (i = 0; i < className.length; i++) {
+    for (let i = 0; i < className.length; i++) {
       
       className[i].style.background = "rgba("+colorCode[randomNumber]+")";
     }
@@ -130,173 +128,210 @@ export default {
 </script>
 
 
-<style lang="sass">
-*
-  margin: 0
-  padding: 0
-  box-sizing: border-box
-  
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-body
-  font-family: "montserrat", sans-serif
+body {
+  font-family: "montserrat", sans-serif;
+}
 
-#app
-  background-image: url("./assets/cold-bg.jpg")
-  background-size: cover
-  background-position: bottom
-  transition: 0.4s
+#app {
+  background-image: url("./assets/cold-bg.jpg");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
 
-  &.warm
-    background-image: url("./assets/warm-bg.jpg")
+#app.warm {
+  background-image: url("./assets/warm-bg.jpg");
+}
 
-main
-  min-height: 100vh
-  padding: 25px
-  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75))
-  
-.search-box
-  width: 100%
-  margin-bottom: 30px
+.logoDiv
+{
+  background-image: url("./assets/warm-bg.jpg");
+  margin: auto;
+  width: 15%;
+  border: 3px solid green;
+  padding: 10px;
+  height: 100px;
+}
 
-  .search-bar
-    display: block
-    width: 100%
-    padding: 15px
-    color: #313131
-    font-size: 20px
-    appearance: none
-    border: none
-    outline: none
-    background: none
-    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25)
-    background-color: rgba(255, 255, 255, 0.5)
-    border-radius: 0px 16px 0px 16px
-    transition: 0.4s
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
 
-    &:focus
-      box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25)
-      background-color: rgba(255, 255, 255, 0.75)
-      border-radius: 16px 0px 16px 0px
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
 
-.location-box
-  .location
-    color: #fff
-    font-size: 32px
-    font-weight: 500
-    text-align: center
-    text-shadow: 1px 3px rgba(0, 0, 0, 0.25)
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
 
-  .date
-    color: #fff
-    font-size: 20px
-    font-weight: 300
-    font-style: italic
-    text-align: center
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
 
-.weather-box
-  text-align: center
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
 
-  .temp
-    display: inline-block
-    padding: 10px 25px
-    color: #fff
-    font-size: 102px
-    font-weight: 900
-    text-shadow: 3px 6px rgba(0, 0, 0, 0.25)
-    background-color: rgba(255, 255, 255, 0.25)
-    border-radius: 16px
-    margin: 30px 0px
-    box-shadow: 3px 6px rgba(0, 0, 0, 0.25)
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+.location-box .date {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+.weather-box {
+  text-align: center;
+}
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
 
-  .weather
-    color: #fff
-    font-size: 48px
-    font-weight: 700
-    font-style: italic
-    text-shadow: 3px 6px rgba(0, 0, 0, 0.25)
+.example1.example2.example3.example4 {
+  height: 50px;
+  overflow: hidden;
+  position: relative;
+  width: 25%;
+  float: left;
+}
 
-.example1
-  &.example2.example3.example4
-    height: 50px
-    overflow: hidden
-    position: relative
-    width: 25%
-    float: left
-    
-    
+.example1 h3 {
+  position: absolute;
+  line-height: 50px;
+  color: white;
+  margin-left: 30%;
+  margin-top: 12%;
+  width: 60%;
+  animation: exampleall 1s linear;
+}
 
-  h3
-    position: absolute
-    line-height: 50px
-    color: white
-    margin-left: 35%
-    margin-top: 12%
-    width: 60%
-    animation: exampleall 1s linear
+.example2 h3 {
+  position: absolute;
+  line-height: 50px;
+  color: white;
+  margin-left: 40%;
+  margin-top: 12%;
+  width: 50%;
+  animation: exampleall 1s linear;
+}
 
-.example2 h3
-  position: absolute
-  line-height: 50px
-  color: white
-  margin-left: 45%
-  margin-top: 12%
-  width: 50%
-  animation: exampleall 1s linear
+.example3 h3 {
+  position: absolute;
+  line-height: 50px;
+  color: white;
+  margin-left: 50%;
+  margin-top: 12%;
+  width: 40%;
+  animation: exampleall 1s linear;
+}
 
-.example3 h3
-  position: absolute
-  line-height: 50px
-  color: white
-  margin-left: 55%
-  margin-top: 12%
-  width: 40%
-  animation: exampleall 1s linear
+.example4 h3 {
+  position: absolute;
+  line-height: 50px;
+  color: white;
+  margin-left: 60%;
+  margin-top: 12%;
+  width: 30%;
+  animation: exampleall 1s linear;
+}
 
-.example4 h3
-  position: absolute
-  line-height: 50px
-  color: white
-  margin-left: 65%
-  margin-top: 12%
-  width: 30%
-  animation: exampleall 1s linear
-
-/* Move it (define the animation)
-@keyframes exampleall
-  0%
-    transform: translateX(200%)
-
-  100%
-    transform: translateX(0%)
+/* Move it (define the animation) */
+@keyframes exampleall {
+  0% {
+    transform: translateX(200%);
+  }
+  100% {
+    transform: translateX(0%);
+  }
+}
 
 .ancortag
+{
   color: white
+}
+  
+.ancortag:hover {
+  width: 60px;
+  height: 60px;
+  border-radius: 100%;
+  display: block;
+  color: black;
+}
 
-.ancortag:hover
-  width: 60px
-  height: 60px
-  border-radius: 100%
-  display: block
-  color: black
-
-.copyright
-  color: white
-  margin-top: 17%
-  text-shadow: 0 1px rgba(255, 255, 255, 0.1)
+.copyright {
+  color: white;
+  margin-top: 17%;
+  text-shadow: 0 1px rgba(255, 255, 255, 0.1);
+}
 
 .copyright:hover
-  color: bisque
-
+{
+color: bisque;
+}
+  
 .background
-  background: url("./assets/pexels-pixabay-209831.jpg")
-  height: 100%
-  overflow: hidden
-  position: absolute
-  width: 100%
-
+{
+  background: url("./assets/pexels-pixabay-209831.jpg");
+  height: 100%;
+  overflow: hidden;
+  position: absolute;
+  width: 100%;
+}
+  
 .change
-    overflow: hidden
-    height: 100%
-    width: 100%
+{
+    overflow: hidden;
+    height: 100%;
+    width: 100%;
+}
+  
 </style>
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,46 @@
-module.exports = {
-     entry: './src/app.vue',
-     output: {
-         path: 'C:\\Users\\slsi\\weather-app',
-         filename: 'bundle.js'
-     },
-     module: {
-        rules: [{ test: /\.vue$/, use: 'raw-loader' }],
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports= {
+    mode: 'development',
+    entry: {
+        app: './src/app.vue'
+    },
+    output: {
+        filename: 'js/[name]/[name].js'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.vue$/,
+            loader: 'vue-loader'
+          },
+          {
+            test: /\.css$/,
+            loader: 'css-loader'
+          },
+          {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+              {
+                loader: 'file-loader',
+              },
+            ],
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              "style-loader",
+              // Translates CSS into CommonJS
+              "css-loader",
+              // Compiles Sass to CSS
+              "sass-loader",
+            ],
+          },
+        ]
       },
+      plugins: [
+        // make sure to include the plugin!
+        new VueLoaderPlugin()
+      ]
 }
