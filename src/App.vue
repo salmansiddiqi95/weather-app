f1fde19f2c295d3801df9cb663f1d9722cba46f8
Salman Alam Siddiqi <salman-alam.siddiqi@t-systems.com>
2022-08-13 00:36:39 +0200

    new update

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,71 @@
 <template>
-  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : '' " >
+  <div id="app">
     <div class="background">
-    <div class="change" on="return randomColor()" >
-      <main>
-        <div class="search-box">
-          <input
-            type="text"
-            class="search-bar"
-            placeholder="Search..."
-            v-model="query"
-            @keypress="fetchWeather"
-          />
-        </div>
-        <div class="logoDiv">
-          
-        </div>
-        <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
-          <div class="location-box">
-            <div class="location">
-              {{ weather.name }}, {{ weather.sys.country }}
-            </div>
-            <div class="date">{{ dateBuilder() }}</div>
+      <div class="change" on="return randomColor()">
+        <main>
+          <div class="search-box">
+            <input
+              type="text"
+              class="search-bar"
+              placeholder="Search..."
+              v-model="query"
+              @keypress="fetchWeather"
+            />
           </div>
 
-          <div class="weather-box">
-            <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
-            <div class="weather">{{ weather.weather[0].main }}</div>
-          </div>
-        </div>
-        <div class="FlyInIcons">
-            <div class="example1">
-              <h3>
-                <a class="ancortag" href="#"><i class="fa fa-instagram"></i></a>
-              </h3>
-            </div>
-            <div class="example2">
-              <h3>
-                <a class="ancortag" href="#"><i class="fa fa-twitter"></i></a>
-              </h3>
-            </div>
-            <div class="example3">
-              <h3>
-                <a class="ancortag" href="#"><i class="fa fa-linkedin"></i></a>
-              </h3>
-            </div>
-            <div class="example4">
-              <h3>
-                <a class="ancortag" href="#"><i class="fa fa-codepen"></i></a>
-              </h3>
+          <div>
+            <div class="container">
+              <img src="./assets/weather-logo.png" class="image" />
             </div>
           </div>
-          <div class="copyright">©{{ new Date().getFullYear() }} All rights reserved.</div>
-      </main>
+          <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+            <div class="location-box">
+              <div class="location">
+                {{ weather.name }}, {{ weather.sys.country }}
+              </div>
+              <div class="date">{{ dateBuilder() }}</div>
+            </div>
+
+            <div class="weather-box">
+              <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+              <div class="weather">{{ weather.weather[0].main }}</div>
+            </div>
+          </div>
+
+          <div>
+            <div class="copyright">
+              ©{{ new Date().getFullYear() }} All rights reserved.
+            </div>
+            <div class="FlyInIcons">
+              <div class="example1">
+                <h3>
+                  <a class="ancortag" href="#"
+                    ><i class="fa fa-instagram"></i
+                  ></a>
+                </h3>
+              </div>
+              <div class="example2">
+                <h3>
+                  <a class="ancortag" href="#"><i class="fa fa-twitter"></i></a>
+                </h3>
+              </div>
+              <div class="example3">
+                <h3>
+                  <a class="ancortag" href="#"
+                    ><i class="fa fa-linkedin"></i
+                  ></a>
+                </h3>
+              </div>
+              <div class="example4">
+                <h3>
+                  <a class="ancortag" href="#"><i class="fa fa-codepen"></i></a>
+                </h3>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
-        </div>
   </div>
 </template>
 
@@ -69,12 +81,17 @@ export default {
     };
   },
   mounted() {
-    let colorCode = ["112, 101, 99, 0.7", "149, 35, 14, 0.7", "24, 48, 24, 0.7", "11, 203, 247, 0.7", "163, 203, 41, 0.7"]; // colors
+    let colorCode = [
+      "112, 101, 99, 0.7",
+      "149, 35, 14, 0.7",
+      "24, 48, 24, 0.7",
+      "11, 203, 247, 0.7",
+      "163, 203, 41, 0.7",
+    ]; // colors
     let className = document.getElementsByClassName("change"); // class name to random color
     let randomNumber = Math.floor(Math.random() * 5);
     for (let i = 0; i < className.length; i++) {
-      
-      className[i].style.background = "rgba("+colorCode[randomNumber]+")";
+      className[i].style.background = "rgba(" + colorCode[randomNumber] + ")";
     }
   },
   methods: {
@@ -137,27 +154,6 @@ export default {
 
 body {
   font-family: "montserrat", sans-serif;
-}
-
-#app {
-  background-image: url("./assets/cold-bg.jpg");
-  background-size: cover;
-  background-position: bottom;
-  transition: 0.4s;
-}
-
-#app.warm {
-  background-image: url("./assets/warm-bg.jpg");
-}
-
-.logoDiv
-{
-  background-image: url("./assets/warm-bg.jpg");
-  margin: auto;
-  width: 15%;
-  border: 3px solid green;
-  padding: 10px;
-  height: 100px;
 }
 
 main {
@@ -247,8 +243,8 @@ main {
   position: absolute;
   line-height: 50px;
   color: white;
-  margin-left: 30%;
-  margin-top: 12%;
+  margin-left: 32%;
+  margin-top: 10%;
   width: 60%;
   animation: exampleall 1s linear;
 }
@@ -257,8 +253,8 @@ main {
   position: absolute;
   line-height: 50px;
   color: white;
-  margin-left: 40%;
-  margin-top: 12%;
+  margin-left: 42%;
+  margin-top: 10%;
   width: 50%;
   animation: exampleall 1s linear;
 }
@@ -267,8 +263,8 @@ main {
   position: absolute;
   line-height: 50px;
   color: white;
-  margin-left: 50%;
-  margin-top: 12%;
+  margin-left: 52%;
+  margin-top: 10%;
   width: 40%;
   animation: exampleall 1s linear;
 }
@@ -277,8 +273,8 @@ main {
   position: absolute;
   line-height: 50px;
   color: white;
-  margin-left: 60%;
-  margin-top: 12%;
+  margin-left: 62%;
+  margin-top: 10%;
   width: 30%;
   animation: exampleall 1s linear;
 }
@@ -293,11 +289,10 @@ main {
   }
 }
 
-.ancortag
-{
-  color: white
+.ancortag {
+  color: white;
 }
-  
+
 .ancortag:hover {
   width: 60px;
   height: 60px;
@@ -307,31 +302,70 @@ main {
 }
 
 .copyright {
+  float: left;
   color: white;
   margin-top: 17%;
   text-shadow: 0 1px rgba(255, 255, 255, 0.1);
+  width: 30%;
 }
 
-.copyright:hover
-{
-color: bisque;
+.copyright:hover {
+  color: bisque;
 }
-  
-.background
-{
+
+.background {
   background: url("./assets/pexels-pixabay-209831.jpg");
   height: 100%;
   overflow: hidden;
   position: absolute;
   width: 100%;
 }
-  
-.change
-{
-    overflow: hidden;
-    height: 100%;
-    width: 100%;
+
+.change {
+  overflow: hidden;
+  height: 100%;
+  width: 100%;
 }
-  
+
+.container {
+  margin: auto;
+  position: relative;
+  width: 10%;
+}
+
+.image {
+  display: block;
+  backface-visibility: hidden;
+  width: 100%;
+  height: auto;
+}
+
+.overlay {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  //background-color: #008CBA;
+  overflow: hidden;
+  width: 0;
+  height: 40px;
+  transition: 0.5s ease;
+}
+
+.container:hover .overlay {
+  width: 100%;
+}
+
+.text {
+  white-space: nowrap;
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  overflow: hidden;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+}
 </style>
 
